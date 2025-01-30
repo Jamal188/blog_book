083fd4e6b773a8094d482574edb901c4b85e9f01
jamal188 <mohamad4400m@gmail.com>
2025-01-30 18:03:28 +0000

    Finished!

--- blogs/views.py
+++ blogs/views.py
@@ -29,7 +29,7 @@ def edit_blog_view(request, blog_id):
 
         if form.is_valid():
             form.save()
-            return redirect('blogs:detail_blog_view', blog_id=post.id)
+            return redirect('/users/profile')
     else :
         form = BlogForm(instance=post)
 
@@ -43,14 +43,86 @@ def delete_blog_view(request, blog_id):
     if request.method == 'POST':
         post.delete()
         return redirect('/users/profile')
-    
-
 @login_required
 def detail_blog_view(request, blog_id):
     blog = get_object_or_404(Blog, id=blog_id)
     comment_form = CommentForm()
     context = {'blog': blog,'comment_form': comment_form,}
     return render(request, 'blogs/detail_view.html', context)
+
+@login_required
+def detail_blog_view22(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    comments = blog.comment_set.all().order_by('-date_created')
+    
+    # Handle like POST
+    if request.method == 'POST' and 'like' in request.POST:
+        if request.user.is_authenticated:
+            if request.user in blog.likes.all():
+                blog.likes.remove(request.user)
+            else:
+                blog.likes.add(request.user)
+        return redirect('blogs:detail_blog_view22', pk=blog.id)
+    
+    # Handle comment POST
+    if request.method == 'POST' and 'comment' in request.POST:
+        form = CommentForm(request.POST)
+        if form.is_valid() and request.user.is_authenticated:
+            Comment.objects.create(
+                blog=blog,
+                user=request.user,
+                content_comment=form.cleaned_data['content_comment']
+            )
+            return redirect('blogs:detail_blog_view22', pk=blog.id)
+    else:
+        form = CommentForm()
+
+    context = {
+        'blog': blog,
+        'comment_form': form,
+        'comments': comments[:1]  # Show first comment initially
+    }
+    return render(request, 'blogs/detail_view22.html', context)
+
+
+
+def welcome_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    return render(request, 'blogs/welcome.html')
+
+@login_required
+def home(request):
+    blogs = Blog.objects.all().order_by('-date_created')
+    return render(request, 'blogs/home.html', {'blogs':blogs})
+
+
+
+
+@login_required
+def load_blogs(request):
+    posts = Blog.objects.all().order_by('-date_created') # getting all the blogs posts from the db
+    posts_data = []
+    for post in posts:
+        posts_data.append({
+            "title": post.title,
+            "content": post.content,
+            "author": post.author.username,  # Include the author's username
+            "date_created": post.date_created.isoformat(),  # Format the date
+        })
+    return JsonResponse(posts_data, safe=False)  # Return the custom JSON response
+    
+
+
+
+
+@login_required
+def load_comments(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+    comments = blog.comments.all().order_by('-date_created_comment')
+    comment_form = CommentForm()
+    context = {'comments': comments , 'comment_form':comment_form, 'blog':blog}
+    return render(request, 'blogs/comments.html', context)
 
 
 @login_required
@@ -93,81 +165,7 @@ def toggle_like(request, blog_id):
     return render(request, 'blogs/partials/like_count.html', context)
 
 
-@login_required
-def detail_blog_view22(request, blog_id):
-    blog = get_object_or_404(Blog, id=blog_id)
-    comments = blog.comments.all().order_by('-date_created_comment')  # Get all comments
-    likes = blog.likes.all()  # Get all likes
 
-    if request.method == 'POST':
-        # Handle comment submission
-        if 'comment' in request.POST:
-            comment_form = CommentForm(request.POST)
-            if comment_form.is_valid():
-                comment = comment_form.save(commit=False)
-                comment.blog = blog
-                comment.user = request.user
-                comment.save()
-                return redirect('blogs:detail_blog_view22', blog_id=blog.id)
-
-        # Handle like submission
-        elif 'like' in request.POST:
-            like, created = Like.objects.get_or_create(blog=blog, user=request.user)
-            if not created:
-                like.delete()  # Unlike if already liked
-            return redirect('blogs:detail_blog_view22', blog_id=blog.id)
-
-    else:
-        comment_form = CommentForm()
-
-    return render(request, 'blogs/detail_view22.html', {
-        'blog': blog,
-        'comments': comments,
-        'likes': likes,
-        'comment_form': comment_form,
-    })
-
-
-
-
-
-def welcome_view(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    return render(request, 'blogs/welcome.html')
-
-@login_required
-def load_blogs(request):
-    posts = Blog.objects.all().order_by('-date_created') # getting all the blogs posts from the db
-    posts_data = []
-    for post in posts:
-        posts_data.append({
-            "title": post.title,
-            "content": post.content,
-            "author": post.author.username,  # Include the author's username
-            "date_created": post.date_created.isoformat(),  # Format the date
-        })
-    return JsonResponse(posts_data, safe=False)  # Return the custom JSON response
-    
-
-
-@login_required
-def home(request):
-    blogs = Blog.objects.all().order_by('-date_created')
-    return render(request, 'blogs/home.html', {'blogs':blogs})
-
-
-
-
-
-
-@login_required
-def load_comments(request, blog_id):
-    blog = get_object_or_404(Blog, id=blog_id)
-    comments = blog.comments.all().order_by('-date_created_comment')
-    comment_form = CommentForm()
-    context = {'comments': comments , 'comment_form':comment_form, 'blog':blog}
-    return render(request, 'blogs/comments.html', context)
 
 @login_required
 def load_likes(request, blog_id):
